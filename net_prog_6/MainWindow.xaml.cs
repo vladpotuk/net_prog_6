56ff03d398113c91100fc9dac723f294888b1514
Потук Владислав Олексійович <143324690+vladpotuk@users.noreply.github.com>
2024-06-25 19:52:00 +0300

    update

--- net_prog_6/MainWindow.xaml.cs
+++ net_prog_6/MainWindow.xaml.cs
@@ -1,24 +1,54 @@
-﻿using System.Text;
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace net_prog_6
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        private const int Port = 8888;
+        private UdpClient udpServer;
+
         public MainWindow()
         {
             InitializeComponent();
+            StartServer();
+        }
+
+        private void StartServer()
+        {
+            udpServer = new UdpClient(Port);
+            udpServer.BeginReceive(ReceiveCallback, null);
+            Log("Server started. Listening on port " + Port);
+        }
+
+        private void ReceiveCallback(IAsyncResult ar)
+        {
+            IPEndPoint clientEndPoint = new IPEndPoint(IPAddress.Any, Port);
+            byte[] clientMessage = udpServer.EndReceive(ar, ref clientEndPoint);
+            string message = Encoding.ASCII.GetString(clientMessage);
+
+            
+
+           
+            string response = "Recipe: Salad\nIngredients: Lettuce, Tomato, Cucumber";
+            byte[] responseData = Encoding.ASCII.GetBytes(response);
+            udpServer.Send(responseData, responseData.Length, clientEndPoint);
+
+            Log($"Response sent to {clientEndPoint}: {response}");
+
+           
+            udpServer.BeginReceive(ReceiveCallback, null);
+        }
+
+        private void Log(string message)
+        {
+            Dispatcher.Invoke(() =>
+            {
+                txtLog.AppendText($"{DateTime.Now}: {message}\n");
+            });
         }
     }
 }
